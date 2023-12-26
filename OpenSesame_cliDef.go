27a9919a5862664c05e4684de4994c7b07682685
Tong Sun <suntong@cpan.org>
2023-12-25 23:27:34 -0500

    - [#] update cli def; add -mg media galley

--- OpenSesame_cliDef.go
+++ OpenSesame_cliDef.go
@@ -8,6 +8,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 ////////////////////////////////////////////////////////////////////////////
@@ -17,11 +18,12 @@ const progname = "OpenSesame" // os.Args[0]
 
 // The Options struct defines the structure to hold the commandline values
 type Options struct {
-	Host  string // host name preferred for external access
-	Port  string // listening port
-	Path  string // path to serve files from
-	Fixed int    // fixed web path # to serve files with
-	Help  bool   // show usage help
+	Host        string // host name preferred for external access
+	Port        string // listening port
+	Path        string // path to serve files from
+	Fixed       int    // fixed web path # to serve files with
+	MediaGalley bool   // as media galley backend server
+	Help        bool   // show usage help
 }
 
 ////////////////////////////////////////////////////////////////////////////
@@ -44,6 +46,8 @@ func initVars() {
 		"path to serve files from")
 	flag.IntVar(&Opts.Fixed, "fx", 0,
 		"fixed web path # to serve files with")
+	flag.BoolVar(&Opts.MediaGalley, "mg", false,
+		"as media galley backend server")
 	flag.BoolVar(&Opts.Help, "help", false,
 		"show usage help")
 }
@@ -51,17 +55,26 @@ func initVars() {
 func initVals() {
 	exists := false
 	// Now override those default values from environment variables
-	if len(Opts.Host) == 0 ||
+	if len(Opts.Host) == 0 &&
 		len(os.Getenv("OPENSESAME_HOST")) != 0 {
 		Opts.Host = os.Getenv("OPENSESAME_HOST")
 	}
-	if len(Opts.Port) == 0 ||
+	if len(Opts.Port) == 0 &&
 		len(os.Getenv("OPENSESAME_PORT")) != 0 {
 		Opts.Port = os.Getenv("OPENSESAME_PORT")
 	}
-	if len(Opts.Path) == 0 ||
+	if len(Opts.Path) == 0 &&
 		len(os.Getenv("OPENSESAME_PATH")) != 0 {
 		Opts.Path = os.Getenv("OPENSESAME_PATH")
+	}
+	if Opts.Fixed == 0 &&
+		len(os.Getenv("OPENSESAME_FX")) != 0 {
+		if i, err := strconv.Atoi(os.Getenv("OPENSESAME_FX")); err == nil {
+			Opts.Fixed = i
+		}
+	}
+	if _, exists = os.LookupEnv("OPENSESAME_MG"); Opts.MediaGalley || exists {
+		Opts.MediaGalley = true
 	}
 	if _, exists = os.LookupEnv("OPENSESAME_HELP"); Opts.Help || exists {
 		Opts.Help = true
@@ -69,7 +82,7 @@ func initVals() {
 
 }
 
-const usageSummary = "  -host\thost name preferred for external access (OPENSESAME_HOST)\n  -port\tlistening port (OPENSESAME_PORT)\n  -path\tpath to serve files from (OPENSESAME_PATH)\n  -fx\tfixed web path # to serve files with (OPENSESAME_FX)\n  -help\tshow usage help (OPENSESAME_HELP)\n\nDetails:\n\n"
+const usageSummary = "  -host\thost name preferred for external access (OPENSESAME_HOST)\n  -port\tlistening port (OPENSESAME_PORT)\n  -path\tpath to serve files from (OPENSESAME_PATH)\n  -fx\tfixed web path # to serve files with (OPENSESAME_FX)\n  -mg\tas media galley backend server (OPENSESAME_MG)\n  -help\tshow usage help (OPENSESAME_HELP)\n\nDetails:\n\n"
 
 // Usage function shows help on commandline usage
 func Usage() {
@@ -79,6 +92,16 @@ func Usage() {
 	fmt.Fprintf(os.Stderr, usageSummary)
 	flag.PrintDefaults()
 	fmt.Fprintf(os.Stderr,
-		"\nWill serve the files from the given path via web server\nof the given port using a one-time random path.\n- downloads are served from such path + 'd'\n- upload is also possible from such path + 'u'\n\nExit and restart will serve from another random path.\n\nThe `-host`, `-port` / `-path` can be overridden by environment variable(s)\n `OPENSESAME_HOST`, `OPENSESAME_PORT` / `OPENSESAME_PATH`\n")
+		`
+Will serve the files from the given path via web server
+of the given port using a one-time random path.
+- downloads are served from such path + 'd'
+- upload is also possible from such path + 'u'
+
+Exit and restart will serve from another random path.
+
+The '-host', '-port' / '-path' can be overridden by environment variable(s)
+'OPENSESAME_HOST', 'OPENSESAME_PORT' / 'OPENSESAME_PATH'
+`)
 	os.Exit(0)
 }
