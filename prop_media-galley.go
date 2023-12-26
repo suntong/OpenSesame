feb0abd43d7c653018fb8f665798e0117ccfed41
Tong Sun <suntong@cpan.org>
2023-12-26 15:32:19 -0500

    - [+] add prop_media-galley.go, basic filepath.Walk

---
+++ prop_media-galley.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// dirPath: Directory to walk through
+func listMedias(dirPath, p string) string {
+	var files []string
+
+	// Walk through the directory and collect file names
+	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println("Error walking through directory:", err)
+		return ""
+	}
+
+	b := &strings.Builder{}
+	fmt.Fprintf(b, "[")
+	for i, f := range files {
+		if i != 0 {
+			fmt.Fprintf(b, ",")
+		}
+		fmt.Fprintf(b, "{src: '%s/%s'}", p, f)
+	}
+	fmt.Fprintf(b, "]")
+	return b.String()
+}
